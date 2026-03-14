7545164255a86eb36b94f9e0f2f34e1dc7362447
izadimrantan <izadimrantan@gmail.com>
2026-03-15 00:40:57 +0800

    third commit

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
-    directUrl: process.env["DATABASE_URL_UNPOOLED"],
+    url: process.env["DATABASE_URL_UNPOOLED"] || process.env["DATABASE_URL"],
   },
 });
