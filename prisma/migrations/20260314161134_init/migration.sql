845307f524108996e38e55966c969155f62b9f08
izadimrantan <izadimrantan@gmail.com>
2026-03-15 00:24:04 +0800

    second commit

--- prisma/migrations/20260314161134_init/migration.sql
+++ prisma/migrations/20260314161134_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "SigningSession" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "documentName" TEXT NOT NULL,
     "walletAddress" TEXT NOT NULL,
     "signerName" TEXT NOT NULL,
@@ -11,8 +11,10 @@ CREATE TABLE "SigningSession" (
     "digitalSignature" TEXT NOT NULL,
     "signerPublicKey" TEXT NOT NULL,
     "txHash" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "completedAt" DATETIME
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "completedAt" TIMESTAMP(3),
+
+    CONSTRAINT "SigningSession_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
